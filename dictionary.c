44d39be02c0bb7d186b7f32d5119c56a723a775a
Brian Scott Goffman <brian.goffman+1@gmail.com>
2017-03-05 23:31:54 +0000

    Add hash, load, check

--- dictionary.c
+++ dictionary.c
@@ -2,25 +2,137 @@
  * Implements a dictionary's functionality.
  */
 
-#include <stdbool.h>
+//* Note: Hash table from Wikipedia - https://en.wikipedia.org/wiki/Hash_function
+// ***************************************************************************/
 
+#include <stdbool.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
 #include "dictionary.h"
+
+// Set size of hashtable
+#define SIZE 1000000
+
+// Creat nodes for linked list
+typedef struct node
+{
+    char word[LENGTH+1];
+    struct node* next;
+}
+node;
+
+// Create hashtable
+node* hashtable[SIZE] = {NULL};
+
+// Create hash function
+int hash (const char* word)
+{
+    int hash = 0;
+    int n;
+    for (int i = 0; word[i] != '\0'; i++)
+    {
+        // if alpha character
+        if(isalpha(word[i]))
+            n = word[i] - 'a' + 1;
+            
+        // if comma
+        else
+            n = 27;
+            
+        hash = ((hash << 3) + n) % SIZE;
+    }
+    return hash;
+}
+
+// Create global variable to count size
+int dictionarySize = 0;
+
+// * Loads dictionary into memory. Returns true if successful else false.
+// * Returns true if word is in dictionary else false.
+ 
+bool load(const char* dictionary)
+{
+    // Opens dictionary
+    FILE* file = fopen(dictionary, "r");
+    if (file == NULL)
+       return false;
+       
+   // Create an array for word to be stored
+   char word[LENGTH+1];
+   
+   // Scan through file, loading each word into hash table
+   while (fscanf(file, "%s\n", word) != EOF)
+   {
+        // Add to dictionary size
+        dictionarySize++;
+        
+        // Allocate memory for new word
+        node* newWord = malloc(sizeof(node));
+        
+        // Add word into new node
+        strcpy(newWord->word, word);
+        
+        // ID what index for word
+        int index = hash(word);
+        
+        // If hashtable is empty insert NULL
+        if (hashtable[index] == NULL)
+        {
+            hashtable[index] = newWord;
+            newWord->next = NULL;
+        }
+        else
+        {
+            newWord->next = hashtable[index];
+            hashtable[index] = newWord;
+        }
+    }
+    
+    // Close dictionary file
+    fclose(file);
+    
+    // Return true if success
+    return true;
+}
 
 /**
  * Returns true if word is in dictionary else false.
  */
-bool check(const char *word)
+bool check(const char* word)
 {
-    // TODO
-    return false;
-}
-
-/**
- * Loads dictionary into memory. Returns true if successful else false.
- */
-bool load(const char *dictionary)
-{
-    // TODO
+    // Creates temp variable to store lower-case word version
+    char temp[LENGTH+1];
+    int len = strlen(word);
+    for (int i = 0; i < len; i++)
+        temp[i] = tolower(word[i]);
+    temp[len] = '\0';
+    
+    // Calculate what index of array for word
+    int index = hash(temp);
+    
+    // If hashtable is empty, return false
+    if (hashtable[index] == NULL)
+    {
+        return false;
+    }
+    
+    // Cursor to compare to word
+    node* cursor = hashtable[index];
+    
+    // Iterate through table unless hashtable is NULL
+    while (cursor != NULL)
+    {
+        if (strcmp(temp, cursor->word) == 0)
+        {
+            return true;
+        }
+        cursor = cursor->next;
+    }
+    
+    // If word isn't found return false
     return false;
 }
 
